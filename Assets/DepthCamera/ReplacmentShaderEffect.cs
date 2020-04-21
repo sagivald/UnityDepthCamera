79aa699cc1922e3def7bfc599f970732d2bb1fc5
Sagi Vald <sagivald@gmail.com>
2020-04-21 12:22:17 +0300

    Depth Camera Commit
    Unity version 2019.3.4f.
    Classic render pipline.
    depth camera using replacment shader.
    far is black near is white

---
+++ Assets/DepthCamera/ReplacmentShaderEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class ReplacmentShaderEffect : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float distance = 10.0f;
+    public Shader ReplacmentShader;
+    void OnEnable()
+    {
+        if (ReplacmentShader != null)
+        {
+            GetComponent<Camera>().SetReplacementShader(ReplacmentShader, "RenderType");
+            GetComponent<Camera>().farClipPlane = distance;
+        }
+    }
+
+    void OnDisable()
+    {
+            GetComponent<Camera>().ResetReplacementShader();
+
+    }
+}
