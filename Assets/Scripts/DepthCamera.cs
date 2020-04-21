e19c9e73ed254ffbbef76cf569ac225039667aec
Sagi Vald <sagivald@gmail.com>
2020-04-21 14:50:43 +0300

    Added DepthCamera
    Added DepthCamera with a depth shader, different script than using replacment shader

---
+++ Assets/Scripts/DepthCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthCamera : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float distance = 10.0f;
+	Material material;
+	float depthLevel = 1;
+	Matrix4x4 projMat;
+	public Camera depthCam;
+	RenderTexture depthImage;
+	public Shader Shade;
+
+    void Start()
+    {
+		depthCam = GetComponent<Camera>();
+		material = new Material(Shade);
+		projMat = depthCam.projectionMatrix;
+		//autoFOV();
+
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (Shade != null)
+        {
+            material.SetFloat("_DepthLevel", depthLevel);
+            Graphics.Blit(src, dest, material);
+            depthCam.farClipPlane = distance;
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
